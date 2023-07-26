c5cdc1a76c4bd08b48e254031ee6d4063790c2fd
yullg <yullg@yullgdeAir.gw.gcableconfig.local>
2023-07-26 13:03:10 +0800

    none

--- lib/src/helper/uri_helper.dart
+++ lib/src/helper/uri_helper.dart
@@ -7,18 +7,42 @@ class UriHelper {
           String? path,
           Map<String, dynamic>? queryParameters,
           String? fragment}) =>
-      Uri(
-        scheme: scheme ?? uri.scheme,
-        userInfo: userInfo ?? uri.userInfo,
-        host: host ?? uri.host,
-        port: port ?? uri.port,
-        path: path ?? uri.path,
-        queryParameters: {
-          ...uri.queryParameters,
-          ...?queryParameters,
-        },
-        fragment: fragment ?? uri.fragment,
+      uri.replace(
+        scheme: scheme,
+        userInfo: userInfo,
+        host: host,
+        port: port,
+        path: path,
+        queryParameters: queryParameters != null
+            ? {
+                ...uri.queryParameters,
+                ...queryParameters,
+              }
+            : queryParameters,
+        fragment: fragment,
       );
+
+  static bool equals(Uri uri,
+          {String? scheme,
+          String? userInfo,
+          String? host,
+          int? port,
+          String? path,
+          Iterable<String>? pathSegments,
+          String? query,
+          Map<String, dynamic>? queryParameters,
+          String? fragment}) =>
+      uri ==
+      uri.replace(
+          scheme: scheme,
+          userInfo: userInfo,
+          host: host,
+          port: port,
+          path: path,
+          pathSegments: pathSegments,
+          query: query,
+          queryParameters: queryParameters,
+          fragment: fragment);
 
   UriHelper._();
 }
