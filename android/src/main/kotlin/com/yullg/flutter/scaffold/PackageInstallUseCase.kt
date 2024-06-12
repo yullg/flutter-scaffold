f6b75e23de722605e96fd41fb640d852a0e8bf85
yullg <yullg@email.com>
2024-06-12 17:50:47 +0800

    none

--- android/src/main/kotlin/com/yullg/flutter/scaffold/PackageInstallUseCase.kt
+++ android/src/main/kotlin/com/yullg/flutter/scaffold/PackageInstallUseCase.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.PackageInstaller
 import android.net.Uri
+import android.util.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import kotlinx.coroutines.Dispatchers
@@ -17,31 +18,39 @@ class PackageInstallUseCase(
 
     fun install(useCaseContext: UseCaseContext) {
         GlobalScope.launch(Dispatchers.IO) {
+            var session: PackageInstaller.Session? = null
             try {
                 val packageInstaller =
                     requiredFlutterPluginBinding.applicationContext.packageManager.packageInstaller
                 val sessionParams =
                     PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL)
                 val sessionId = packageInstaller.createSession(sessionParams)
-                packageInstaller.openSession(sessionId).use { session ->
-                    session.openWrite("app", 0, -1).use { output ->
-                        requiredFlutterPluginBinding.applicationContext.contentResolver.openInputStream(
-                            Uri.parse(useCaseContext.call.argument("uri"))
-                        )?.use { input ->
-                            input.copyTo(output)
+                session = packageInstaller.openSession(sessionId)
+                session.openWrite("app", 0, -1).use { output ->
+                    requiredFlutterPluginBinding.applicationContext.contentResolver.openInputStream(
+                        Uri.parse(useCaseContext.call.argument("uri"))
+                    )?.use { input ->
+                        input.copyTo(output).let {
+                            Log.i("PackageInstallUseCase", "apk size: $it")
                         }
                     }
-                    val pendingIntent = PendingIntent.getBroadcast(
-                        requiredFlutterPluginBinding.applicationContext,
-                        sessionId,
-                        Intent("${requiredFlutterPluginBinding.applicationContext.packageName}.INSTALL_COMPLETE"),
-                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-                    )
-                    session.commit(pendingIntent.intentSender)
                 }
+                val pendingIntent = PendingIntent.getBroadcast(
+                    requiredFlutterPluginBinding.applicationContext,
+                    sessionId,
+                    Intent("${requiredFlutterPluginBinding.applicationContext.packageName}.SESSION_API_PACKAGE_INSTALLED"),
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+                session.commit(pendingIntent.intentSender)
                 useCaseContext.result.success(null)
             } catch (e: Throwable) {
-                useCaseContext.result.error(ERROR_CODE, e.message, null)
+                try {
+                    session?.abandon()
+                } catch (e: Throwable) {
+                    Log.e("PackageInstallUseCase", "failed to abandon session", e)
+                } finally {
+                    useCaseContext.result.error(ERROR_CODE, e.message, null)
+                }
             }
         }
     }
