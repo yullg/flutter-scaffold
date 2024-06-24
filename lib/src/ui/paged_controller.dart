463420663e228003704e318e3161b3e4d4c5a9ae
yullg <yullg@email.com>
2024-06-24 18:30:00 +0800

    none

--- lib/src/ui/paged_controller.dart
+++ lib/src/ui/paged_controller.dart
@@ -18,6 +18,12 @@ abstract class PagedController<T> extends ChangeNotifier {
   })  : _data = [...?initialData],
         _status = initialStatus ?? PagedControllerStatus.idle;
 
+  /// 修改已加载的数据并通知监听器更新
+  void changeData(void Function(List<T> data) block) {
+    block(_data);
+    notifyListeners();
+  }
+
   void loadMoreData() {
     try {
       switch (_status) {
