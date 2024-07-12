9caa5872cef3d736d9de1915602e8133d476205c
yullg <yullg@email.com>
2024-07-12 12:11:10 +0800

    none

--- lib/src/helper/uuid_helper.dart
+++ lib/src/helper/uuid_helper.dart
@@ -1,11 +1,9 @@
 import 'package:uuid/uuid.dart';
 
 class UuidHelper {
-  static final _uuid = Uuid();
+  static String v1() => const Uuid().v1();
 
-  static String v1() => _uuid.v1();
-
-  static String v4() => _uuid.v4();
+  static String v4() => const Uuid().v4();
 
   UuidHelper._();
 }
