e4b2ba6c7de92623516c4ec425e21ad500ccdc7c
yullg <yullg@email.com>
2024-08-27 15:29:22 +0800

    none

--- ios/Classes/ScaffoldPlugin.swift
+++ ios/Classes/ScaffoldPlugin.swift
@@ -4,6 +4,7 @@ public class ScaffoldPlugin: NSObject, FlutterPlugin {
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         DocumentPickerPlugin.register(with: registrar)
+        PhotoLibraryPlugin.register(with: registrar)
         FileManagerPlugin.register(with: registrar)
         URLPlugin.register(with: registrar)
     }
