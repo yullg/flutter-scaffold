f9d49b4f453feea987681472539e73a61bd9d267
yullg <yullg@foxmail.com>
2025-05-10 01:26:46 +0800

    none

--- android/src/main/kotlin/com/yullg/flutter/scaffold/MediaProjectionUseCase.kt
+++ android/src/main/kotlin/com/yullg/flutter/scaffold/MediaProjectionUseCase.kt
@@ -2,6 +2,8 @@ package com.yullg.flutter.scaffold
 
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
+import androidx.core.content.ContextCompat
+import com.yullg.flutter.scaffold.android.MediaProjectionService
 import com.yullg.flutter.scaffold.core.BaseUseCase
 import com.yullg.flutter.scaffold.core.RequestCode
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -13,9 +15,7 @@ object MediaProjectionUseCase : BaseUseCase(
     methodChannelName = "com.yullg.flutter.scaffold/media_projection"
 ), PluginRegistry.ActivityResultListener {
 
-    var mediaProjectionToken: MediaProjectionToken? = null
-        private set
-
+    private var mediaProjectionToken: MediaProjectionToken? = null
     private var authorizeResult: MethodChannel.Result? = null
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
@@ -41,6 +41,29 @@ object MediaProjectionUseCase : BaseUseCase(
                     RequestCode.MEDIA_PROJECTION_CREATE_SCI.code
                 )
                 authorizeResult = result
+            }
+
+            "startAudioPlaybackCapture" -> {
+                val activity = requiredActivityPluginBinding.activity
+                val notificationJson = call.argument<String>("notification")!!
+                val recorderJson = call.argument<String>("recorder")!!
+                val token = mediaProjectionToken
+                if (token != null) {
+                    val intent = Intent(activity, MediaProjectionService::class.java).apply {
+                        putExtra(
+                            "action",
+                            MediaProjectionService.ACTION_START_AUDIO_PLAYBACK_CAPTURE
+                        )
+                        putExtra("tokenResultCode", token.resultCode)
+                        putExtra("tokenData", token.data)
+                        putExtra("notificationJson", notificationJson)
+                        putExtra("recorderJson", recorderJson)
+                    }
+                    ContextCompat.startForegroundService(activity, intent)
+                    result.success(null)
+                } else {
+                    result.error(ERROR_CODE, "Not found media projection token!", null)
+                }
             }
 
             else -> {
@@ -72,3 +95,5 @@ object MediaProjectionUseCase : BaseUseCase(
 }
 
 data class MediaProjectionToken(val resultCode: Int, val data: Intent?)
+
+private const val ERROR_CODE = "MediaProjectionUseCaseError"
--- lib/src/plugin/android/android_media_projection_plugin.dart
+++ lib/src/plugin/android/android_media_projection_plugin.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 class AndroidMediaProjectionPlugin {
@@ -6,6 +8,28 @@ class AndroidMediaProjectionPlugin {
 
   static Future<void> authorize() {
     return _methodChannel.invokeMethod<void>("authorize");
+  }
+
+  static Future<void> startAudioPlaybackCapture({
+    required int notificationId,
+    required String notificationChannelId,
+    String? notificationContentTitle,
+    String? notificationContentText,
+    int sampleRate = 44100,
+    bool stereo = true,
+  }) {
+    return _methodChannel.invokeMethod<void>("startAudioPlaybackCapture", {
+      "notification": jsonEncode({
+        "id": notificationId,
+        "channelId": notificationChannelId,
+        "contentTitle": notificationContentTitle,
+        "contentText": notificationContentText,
+      }),
+      "recorder": jsonEncode({
+        "sampleRate": sampleRate,
+        "stereo": stereo,
+      }),
+    });
   }
 
   AndroidMediaProjectionPlugin._();
