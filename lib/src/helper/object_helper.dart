617d26d2d93ebf104c1e4307c10a13f6e2ce67b3
yullg <yullg@foxmail.com>
2025-07-20 21:59:55 +0800

    none

---
+++ lib/src/helper/object_helper.dart
@@ -0,0 +1,19 @@
+class ObjectHelper {
+  static R let<T, R>(T obj, R Function(T it) block) => block(obj);
+
+  static R? letIf<T, R>(T obj, bool Function(T value) predicate, R Function(T it) block) =>
+      predicate(obj) ? block(obj) : null;
+
+  static R? letUnless<T, R>(T obj, bool Function(T value) predicate, R Function(T it) block) =>
+      predicate(obj) ? null : block(obj);
+
+  static R? letIfNoNull<T, R>(T? obj, R Function(T it) block) => obj != null ? block(obj) : null;
+
+  static void also<T>(T obj, void Function(T it) block) => block(obj);
+
+  static T? takeIf<T>(T obj, bool Function(T value) predicate) => predicate(obj) ? obj : null;
+
+  static T? takeUnless<T>(T obj, bool Function(T value) predicate) => predicate(obj) ? null : obj;
+
+  ObjectHelper._();
+}
