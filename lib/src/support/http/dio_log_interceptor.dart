158dba08462c6136675ca05994b96940a46bd319
yullg <yullg@email.com>
2025-12-31 15:04:00 +0800

    none

--- lib/src/support/http/dio_log_interceptor.dart
+++ lib/src/support/http/dio_log_interceptor.dart
@@ -1,46 +1,120 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
-import '../logger/default_logger.dart';
-import '../logger/log.dart';
-import '../logger/logger.dart';
+import '../../internal/scaffold_logger.dart';
 
-class DioLogInterceptor extends LogInterceptor {
-  final DioLogPrinter? _logPrinter;
+const _kRequestBeginTime = "ScaffoldRequestBeginTime";
 
-  DioLogInterceptor({
-    super.request,
-    super.requestHeader,
-    super.requestBody,
-    super.responseHeader,
-    super.responseBody,
-    super.error,
-    DioLogPrinter? logPrinter,
-  }) : _logPrinter = logPrinter {
-    logPrint = _doLogPrint;
-  }
+class DioLogInterceptor extends Interceptor {
+  final void Function(String log) logPrint;
 
-  void _doLogPrint(Object object) => _logPrinter?.print(object);
-}
+  final bool requestHeader;
+  final bool requestBody;
+  final bool responseHeader;
+  final bool responseBody;
 
-abstract interface class DioLogPrinter {
-  void print(Object object);
-}
-
-class LoggerDioLogPrinter implements DioLogPrinter {
-  final ILogger _logger;
-  final LogLevel _level;
-
-  LoggerDioLogPrinter({String? name, LogLevel level = LogLevel.debug})
-    : _logger = name != null ? Logger(name) : DefaultLogger(),
-      _level = level;
+  const DioLogInterceptor({
+    required this.logPrint,
+    this.requestHeader = false,
+    this.requestBody = true,
+    this.responseHeader = false,
+    this.responseBody = true,
+  });
 
   @override
-  void print(Object object) => switch (_level) {
-    LogLevel.trace => _logger.trace(object),
-    LogLevel.debug => _logger.debug(object),
-    LogLevel.info => _logger.info(object),
-    LogLevel.warn => _logger.warn(object),
-    LogLevel.error => _logger.error(object),
-    LogLevel.fatal => _logger.fatal(object),
-  };
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.extra[_kRequestBeginTime] = DateTime.now();
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    try {
+      final DateTime? beginTime = response.requestOptions.extra[_kRequestBeginTime];
+      final duration = beginTime != null ? DateTime.now().difference(beginTime) : null;
+      final sb = StringBuffer();
+      sb.writeln(
+        "${response.requestOptions.method} ${response.requestOptions.uri} → ${response.statusCode} (${duration?.inMilliseconds ?? '--'}ms)",
+      );
+      if (requestHeader) {
+        sb.writeln("Request-Headers: ${jsonEncode(response.requestOptions.headers, toEncodable: _toJsonEncodable)}");
+      }
+      if (requestBody) {
+        sb.writeln("Request-Body: ${_requestDataToString(response.requestOptions.data)}");
+      }
+      if (responseHeader) {
+        sb.writeln("Response-Headers: ${jsonEncode(response.headers.map, toEncodable: _toJsonEncodable)}");
+      }
+      if (responseBody) {
+        sb.writeln("Response-Body: ${_responseDataToString(response.data)}");
+      }
+      logPrint(sb.toString());
+    } catch (e, s) {
+      ScaffoldLogger().error(null, e, s);
+    }
+    handler.next(response);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    try {
+      final options = err.requestOptions;
+      final DateTime? beginTime = options.extra[_kRequestBeginTime];
+      final duration = beginTime != null ? DateTime.now().difference(beginTime) : null;
+      final sb = StringBuffer();
+      sb.writeln(
+        "${options.method} ${options.uri} → ${err.response?.statusCode ?? 'ERROR'} (${duration?.inMilliseconds ?? '--'}ms)",
+      );
+      if (requestHeader) {
+        sb.writeln("Request-Headers: ${jsonEncode(options.headers, toEncodable: _toJsonEncodable)}");
+      }
+      if (requestBody) {
+        sb.writeln("Request-Body: ${_requestDataToString(options.data)}");
+      }
+      final response = err.response;
+      if (response != null) {
+        if (responseHeader) {
+          sb.writeln("Response-Headers: ${jsonEncode(response.headers.map, toEncodable: _toJsonEncodable)}");
+        }
+        if (responseBody) {
+          sb.writeln("Response-Body: ${_responseDataToString(response.data)}");
+        }
+      }
+      sb.writeln("Error: $err");
+      logPrint(sb.toString());
+    } catch (e, s) {
+      ScaffoldLogger().error(null, e, s);
+    }
+    handler.next(err);
+  }
+
+  Object? _toJsonEncodable(Object? nonEncodable) {
+    return nonEncodable?.toString();
+  }
+
+  String? _requestDataToString(dynamic data) {
+    if (data is FormData) {
+      return jsonEncode(<String, dynamic>{
+        "type": "FormData",
+        "fields": <Map<String, String>>[
+          ...data.fields.map((e) => <String, String>{e.key: e.value}),
+        ],
+        "files": <Map<String, dynamic>>[
+          ...data.files.map(
+            (e) => <String, dynamic>{
+              "key": e.key,
+              "filename": e.value.filename,
+              "contentType": e.value.contentType?.toString(),
+            },
+          ),
+        ],
+      });
+    }
+    return data?.toString();
+  }
+
+  String? _responseDataToString(dynamic data) {
+    return data?.toString();
+  }
 }
