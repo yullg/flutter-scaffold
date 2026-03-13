2902c66cfca3b4e45ff7255db83818c887a0c7de
yullg <yullg@email.com>
2026-03-13 10:54:31 +0800

    none

--- lib/src/sugar/scope_function.dart
+++ lib/src/sugar/scope_function.dart
@@ -1,10 +1,10 @@
 class ScopeFunction {
   static R let<T, R>(T obj, R Function(T it) block) => block(obj);
 
-  static R? letIf<T, R>(T obj, bool Function(T value) predicate, R Function(T it) block) =>
+  static R? letIf<T, R>(T obj, bool Function(T value) predicate, R? Function(T it) block) =>
       predicate(obj) ? block(obj) : null;
 
-  static R? letUnless<T, R>(T obj, bool Function(T value) predicate, R Function(T it) block) =>
+  static R? letUnless<T, R>(T obj, bool Function(T value) predicate, R? Function(T it) block) =>
       predicate(obj) ? null : block(obj);
 
   static R? letIfNotNull<T, R>(T? obj, R? Function(T it) block) => obj != null ? block(obj) : null;
